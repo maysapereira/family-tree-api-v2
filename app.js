2ea2049ebcba2273eb556330dbdc675af9f9bef3
Maysa Pereira <maysalvespereira@gmail.com>
2022-05-15 13:23:18 -0300

    :sparkles: (build) adiciona métodos get e post

--- app.js
+++ app.js
@@ -13,3 +13,21 @@ db.once("open", ()=> {
 
 app.get('/', (req,res)=> res.send("Funcionando"))
 app.listen(port, ()=> console.log(`Servidor rodando na porta: ${port}`))
+
+//
+
+const members = [
+   {id: 1, "name": "Sony"},
+   {id: 2, "name": "Mike"}
+]
+
+app.get('/members', (req, res) => {
+   res.status(200).json(members)
+})
+
+app.post('/members', (req, res) => {
+   members.push(req.body)
+   res.status(201).send('Membro adicionado na árvore genealógica')
+})
+
+export default app
