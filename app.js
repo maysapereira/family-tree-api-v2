050290dce5489030db912aa83d5665c67f50b426
Maysa Pereira <maysalvespereira@gmail.com>
2022-05-15 15:44:55 -0300

    :sparkles: (build) adiciona metodo put

--- app.js
+++ app.js
@@ -30,4 +30,14 @@ app.post('/members', (req, res) => {
    res.status(201).send('Membro adicionado na árvore genealógica')
 })
 
+app.put('/members/:id', (req, res) => {
+   let index = searchMember(req.params.id)
+   members[index].name = req.body.name
+   res.status(200).json(members)
+})
+
+function searchMember(id) {
+   return members.findIndex(members => members.id == id)
+}
+
 export default app
